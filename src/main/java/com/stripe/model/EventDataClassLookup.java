27c61f6b876d9951ccd94b23b8b55e8ce4b1b1c9
Alexey Kalinichenko <akalini@gmail.com>
2019-02-26 17:14:49 -0800

    Adds settlement on top of Stripe SDK.

--- src/main/java/com/stripe/model/EventDataClassLookup.java
+++ src/main/java/com/stripe/model/EventDataClassLookup.java
@@ -175,6 +175,7 @@ final class EventDataClassLookup {
     classLookup.put("issuing.physical_bundle", com.stripe.model.issuing.PhysicalBundle.class);
     classLookup.put("issuing.settlement", com.stripe.model.issuing.Settlement.class);
     classLookup.put("issuing.token", com.stripe.model.issuing.Token.class);
+    classLookup.put("issuing.settlement", com.stripe.model.issuing.Settlement.class);
     classLookup.put("issuing.transaction", com.stripe.model.issuing.Transaction.class);
 
     classLookup.put("radar.early_fraud_warning", com.stripe.model.radar.EarlyFraudWarning.class);
